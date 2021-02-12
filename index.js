db911b62cfb2de5897bd48dccd3a3f1a698e0fba
ssan9 <perrsy@gmail.com>
2021-02-12 01:48:33 -0500

    added edge cases

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 function findPrime(string) {
   let str = string.split(' ');
-  if (str[0] < 1 || str[1] < 1) {
+//   console.log(str);
+  if (str[0] < 0 || str[1] < 0) {
+//       console.log('hi', str[0]);
+
     throw 'Please provide a positive number as negative integers cannot be prime'
   }
   
   let primeList = [];
   
-  let highestOccurringDigit;
-  
-//   if (str[0] === '1' && str[1] === '10') {
-//     return highestOccurringDigit = 7
-//   }
+  let highestOccurringDigit; 
     
   for (let i = +str[0]; i <= +str[1]; i++) {
-    console.log(i);
+//     console.log(i);
     if (i === 2) {
       primeList.push(2);
     }
@@ -36,25 +35,23 @@ function findPrime(string) {
         primeList.push(i);
       }
     }
-//     else {
-//       primeList.push("1", "2", "3", "5", "7");
-// //       highestOccurringDigit = Math.max(primeList);
-//     }
+
   }
   
     
-    console.log(primeList);
+//     console.log(primeList);
   
   if(!primeList || !primeList.length) {
     return 'No prime numbers found';
   }
+
   
-  if (str[0] >= 1 && (str[1] < '8') || str[1] <= '9') {
+  if (+str[0] >= 1 && +str[1] < 11) {
     return Math.max(...primeList);
   }
   
   let occurrence = primeList.join('').split('');
-  console.log('occurrence', occurrence);
+//   console.log('occurrence', occurrence);
   occurrence.sort();
   let count = 1;
   let obj = {};
@@ -69,25 +66,32 @@ function findPrime(string) {
   }
 //     console.log(primeList);
 
-  console.log('obj', obj);
+//   console.log('obj', obj);
 
-//   highestOccurringDigit = Object.keys(obj).reduce((current, previous) => obj[previous] > obj[current] ? previous : current);
-    highestOccurringDigit = Object.keys(obj).reduce((current, previous) => Math.max(current, previous));
+  highestOccurringDigit = Object.keys(obj).reduce((current, previous) => obj[previous] > obj[current] ? previous : current);
+//     highestOccurringDigit = Object.keys(obj).reduce((current, previous) => Math.max(current, previous));
 
   return +highestOccurringDigit;
 }
 
-console.log(findPrime('51 999'));
-console.log(findPrime('8 10'));
-console.log(findPrime('2 10'));
-console.log(findPrime('1 10'));
-console.log(findPrime('1 2'));
-console.log(findPrime('1 20'));
-console.log(findPrime('1 30'));
-console.log(findPrime('1 100'));
+// console.log('51 999', findPrime('51 999'));
+// console.log('8 10', findPrime('8 10'));
+// console.log('2 10', findPrime('2 10'));
+// console.log('1 10', findPrime('1 10'));
+// console.log('1 2', findPrime('1 2'));
+// console.log('1 20', findPrime('1 20'));
+// console.log('1 30', findPrime('1 30'));
+// console.log('1 100', findPrime('1 100'));
+// console.log('1 10', findPrime('1 10'));
+// console.log('1 10', findPrime('1 10'));
+// console.log('2 20', findPrime('2 20'));
+// console.log('11 30', findPrime('11 30'));
+// console.log('21 30', findPrime('21 30'));
+// console.log('1 100', findPrime('1 100'));
+// console.log('-5 -20', findPrime('-5 -20'));
+// console.log('-1 999', findPrime('-1 999'));
 
-
-
+console.log('4 0', findPrime('4 0'));
 
 
 // (""+n).split("")
